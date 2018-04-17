33714788d0b56636e427fcf333d696628e13cb83
MrPooopyButtHol3 <38476370+MrPooopyButtHol3@users.noreply.github.com>
2018-04-17 23:34:02 +0200

    mrpbh

--- MythoCraft/src/main/java/com/mrpooopybutthol3/mythocraft/init/ModItems.java
+++ MythoCraft/src/main/java/com/mrpooopybutthol3/mythocraft/init/ModItems.java
@@ -19,6 +19,4 @@ public class ModItems
 	public static final Item SILVER_INGOT = new ItemBase("silver_ingot");
 	public static final Item METEORIC_IRON_INGOT = new ItemBase("meteoric_iron_ingot");
 	public static final Item ADAMANT = new ItemBase("adamant"); 
-	public static final Item RUBY = new ItemBase("ruby");
-	
 }
